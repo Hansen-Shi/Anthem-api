a0ada37aec61065062b4220c00920d04c87c25c4
Derek Baum <dgbaum@udel.edu>
2020-05-14 00:22:52 -0400

    something something spotify

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
+/* tslint:disable */
 import bodyParser from "body-parser";
 import express from "express";
-
 import {ApiRouter} from "./router";
+import * as querystring from "querystring";
+import * as cors from "cors";
+import * as request from "request";
+import * as cookieParser from "cookie-parser";
+
+const client_id = '1191247894b54b3e9ea7590ed877e4b4'; // Your client id
+const client_secret = 'ba5a2acd5e174889a57ee849a81e92d8'; // Your secret
+const redirect_uri = 'http://localhost:3000/api/hello'; // Your redirect uri
+const stateKey = 'spotify_auth_state';
 
 class Application {
     public app: express.Application;
@@ -36,6 +45,35 @@ class Application {
     // setup routes for the express server
     public buildRoutes(): void {
         this.app.use("/api", new ApiRouter().getRouter());
+    }
+
+    public buildOAuthEndpoints(): void {
+        this.app.get('/login', function(req, res) {
+
+            function generateRandomString(number: number) {
+                var text = '';
+                var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+                for (var i = 0; i < length; i++) {
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                }
+                return text;
+            }
+
+            let state = generateRandomString(16);
+            res.cookie(stateKey, state);
+
+            // your application requests authorization
+            var scope = "user-read-private user-read-email";
+            res.redirect('https://accounts.spotify.com/authorize?' +
+                querystring.stringify({
+                    response_type: 'code',
+                    client_id: client_id,
+                    scope: scope,
+                    redirect_uri: redirect_uri,
+                    state: state
+                }));
+        });
     }
 }
 new Application().start();
